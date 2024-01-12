eeca4585fe30285d93521343e60bb36876ca1e03
Sidhant Arya <121373417+iamsidhant@users.noreply.github.com>
2024-01-12 15:59:29 +0530

    image search

---
+++ script.js
@@ -0,0 +1,7 @@
+
+
+const formEl = document.querySelector("form");
+const searchInputEl = document.getElementById("search-input");
+const searchResults = document.querySelector(".search-results");
+const showMoreButtonEl = document.getElementById("show-more-button");
+
