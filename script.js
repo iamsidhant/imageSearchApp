2cc747fa0a6916eca06910db812f5ee5c4af45d3
Sidhant Arya <121373417+iamsidhant@users.noreply.github.com>
2024-01-14 12:48:01 +0530

    image search

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
-
+const accesskey = "ErAxyHdtPvLRKr1WfKXBVHI_nu6PvTUYKLAXQyfn1IA";
 
 const formEl = document.querySelector("form");
 const searchInputEl = document.getElementById("search-input");
 const searchResults = document.querySelector(".search-results");
 const showMoreButtonEl = document.getElementById("show-more-button");
 
+let inputData = "";
+let page = 1;
+
+formEl.addEventListener("submit", (event) => {
+    event.preventDefault();
+    page = 1;
+    searchImages();
+})
+
+showMoreButtonEl.addEventListener("click", () => {
+    searchImages();
+})
