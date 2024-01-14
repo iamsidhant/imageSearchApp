aacc67eddc0375bd70e39820556516d64240ecdb
Sidhant Arya <121373417+iamsidhant@users.noreply.github.com>
2024-01-14 19:04:49 +0530

    image search project

--- script.js
+++ script.js
@@ -1,12 +1,47 @@
-const accesskey = "ErAxyHdtPvLRKr1WfKXBVHI_nu6PvTUYKLAXQyfn1IA";
+const accessKey = "ErAxyHdtPvLRKr1WfKXBVHI_nu6PvTUYKLAXQyfn1IA";
 
 const formEl = document.querySelector("form");
 const searchInputEl = document.getElementById("search-input");
-const searchResults = document.querySelector(".search-results");
+const searchResultsEl = document.querySelector(".search-results");
 const showMoreButtonEl = document.getElementById("show-more-button");
 
 let inputData = "";
 let page = 1;
+
+async function searchImages(){
+    inputData = searchInputEl.value;
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`;
+
+    const reponse = await fetch(url);
+    const data = await reponse.json();
+    if (page == 1) {
+        searchResultsEl.innerHTML = "";
+    }
+
+    const results = data.results;
+
+    results.map((result) => {
+        const imageWrapper = document.createElement("div");
+        imageWrapper.classList.add("search-result");
+        const image = document.createElement("img");
+        image.src = result.urls.small;
+        image.alt = result.alt_description;
+        const imageLink = document.createElement("a");
+        imageLink.href = result.links.html;
+        imageLink.target = "_blank";
+        imageLink.textContent = result.alt_description;
+
+        imageWrapper.appendChild(image);
+        imageWrapper.appendChild(imageLink);
+        imageWrapper.appendChild(imageWrapper);
+    });
+
+    page++;
+
+    if (page > 1) {
+        showMoreButtonEl.style.display = "block";
+    }
+}
 
 formEl.addEventListener("submit", (event) => {
     event.preventDefault();
